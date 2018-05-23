f9ae5999ddd40ac23d1fdf900d5621b06c328ca3
Smartherd <smartherd@gmail.com>
2018-05-23 13:43:26 +0530

    Section Two, Video Four

--- app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
@@ -20,9 +20,10 @@ class MainActivity : AppCompatActivity() {
 
 		btnSendMsgToNextActivity.setOnClickListener {
 			val message: String = etUserMessage.text.toString()
-			Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-
 			val intent = Intent(this, SecondActivity::class.java)
+
+			intent.putExtra("user_message", message)
+
 			startActivity(intent)
 		}
 
--- app/src/main/java/com/smartherd/msgshareapp/SecondActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/SecondActivity.kt
@@ -2,6 +2,8 @@ package com.smartherd.msgshareapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
@@ -9,5 +11,11 @@ class SecondActivity : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_second)
 
+		val bundle: Bundle? = intent.extras
+		val msg = bundle!!.getString("user_message")
+
+		Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+
+		txvUserMessage.text = msg
 	}
 }
