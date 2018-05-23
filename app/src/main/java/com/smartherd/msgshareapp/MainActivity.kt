3e9108f7b8385d3b2e8e7fb2b40b242a68b6e454
Smartherd <smartherd@gmail.com>
2018-05-23 14:05:44 +0530

    Section Two, Video Five

--- app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
@@ -27,5 +27,17 @@ class MainActivity : AppCompatActivity() {
 			startActivity(intent)
 		}
 
+		btnShareToOtherApps.setOnClickListener {
+
+			val message: String = etUserMessage.text.toString()
+
+			val intent = Intent()
+			intent.action = Intent.ACTION_SEND
+			intent.putExtra(Intent.EXTRA_TEXT, message)
+			intent.type = "text/plain"
+
+			startActivity(Intent.createChooser(intent, "Please select app: "))
+		}
+
 	}
 }
