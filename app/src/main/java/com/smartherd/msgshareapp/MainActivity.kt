71b06f1f905692612df557101dba0ba24d7a35df
Smartherd <smartherd@gmail.com>
2018-05-23 13:01:17 +0530

    Section Two, Video Three

--- app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.smartherd.msgshareapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
@@ -13,10 +14,17 @@ class MainActivity : AppCompatActivity() {
 		setContentView(R.layout.activity_main)
 
 		btnShowToast.setOnClickListener {
-			// Code
 			Log.i("MainActivity", "Button was clicked !")
-
 			Toast.makeText(this, "Button was clicked !", Toast.LENGTH_SHORT).show()
 		}
+
+		btnSendMsgToNextActivity.setOnClickListener {
+			val message: String = etUserMessage.text.toString()
+			Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+			val intent = Intent(this, SecondActivity::class.java)
+			startActivity(intent)
+		}
+
 	}
 }
---
+++ app/src/main/java/com/smartherd/msgshareapp/SecondActivity.kt
@@ -0,0 +1,13 @@
+package com.smartherd.msgshareapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_second)
+
+	}
+}
